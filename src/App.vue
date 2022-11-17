49445c6e0fa28e551576dd5ddcb6b908f96e378a
colopisalvatore <salvatorecolopi1989@gmail.com>
2022-11-17 17:01:04 +0100

    add AppHeder with Intro

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <AppHeader />
+    <main>
+        
+    </main>
+
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+
+export default {
+    components: { AppHeader }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style >
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <nav class="nav-container d-flex justify-content-between align-items-center">
+                <div v-if="show" class="logo-container d-flex justify-content-start align-items-center">
+                        <img @click="backToPopular(popular)" src="../assets/img/boolflix.png" alt="" />
+                </div>
+            <ul>
+                <li>home</li>
+                <li>home</li>
+            </ul>
+        </nav>
+        <div v-if="show" class="search-bar">
+
+            <input type="text" placeholder="Search Movie" v-model="select" @keyup.enter="filterMovie(select)" />
+            <button @click="filterMovie(select)">Search</button>
+
+        </div>
+
+        <transition name="fade">
+            <div v-if="!show" class="video-container">
+
+                <img src="../assets/img/boolflix.gif" alt="" />
+
+            </div>
+        </transition>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+    components: {
+
+    },
+
+    data() {
+        return {
+            select: "",
+        };
+    },
+
+    props: {
+        show: Boolean,
+        popular: Boolean,
+    },
+
+    methods: {
+        filterMovie(select) {
+            this.$emit('movies', select);
+            this.select = "";
+        },
+        backToPopular(popular) {
+            this.$emit('back', popular);
+        },
+    },
+
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/partials/appheader";
+</style>
